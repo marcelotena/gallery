5f1224e95f7ad7384e9de55866c6e68f855cab6a
Marcelo Tena Vicente <marcelotena@gmail.com>
2015-02-17 12:18:11 +0100

    Added gulp task to concatenate JS files

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,15 @@
 var gulp = require('gulp');
     gutil = require('gulp-util');
+    concat = require('gulp-concat');
+
+var jsSources = [
+    'components/scripts/lib.js',
+    'components/scripts/main.js'
+];
+
+gulp.task('js', function() {
+    gulp.src(jsSources)
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('builds/development/js'))
+});
 
